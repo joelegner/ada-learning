3ece1dd1d198f0c2aefe87cffd48bef71d92cfc3
Joe Legner <joelegner@gmail.com>
2022-10-21 06:27:22 -0400

    Added FERC Rule 881 code

--- make.sh
+++ make.sh
@@ -2,3 +2,4 @@
 gprbuild -P embed.gpr
 gprbuild -P roll_dice.gpr
 gprbuild -P numbers.gpr
+gprbuild -P rule881.gpr
---
+++ src/rule881.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Numerics.Discrete_Random;
+with Ada.Float_Text_IO;
+with Ada.Integer_Text_IO;
+
+procedure Rule881 is
+begin
+   Put_Line("FERC Rule 881 Calculator");
+   Put_Line("by Joe Legner 10/21/2022");  
+end Rule881;
