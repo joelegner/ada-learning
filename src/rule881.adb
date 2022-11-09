8fa36d7bb3ceea62bb1fb15b9459f516ee613a26
Joe Legner <joelegner@gmail.com>
2022-11-08 20:10:47 -0500

    Added comments and a pithy phrase

--- src/rule881.adb
+++ src/rule881.adb
@@ -8,11 +8,15 @@ with Ieee738;
 -- 5-degree increments for the most common conductor in the
 -- transmission system
 
+-- What I do to run it as fast as possible is:
+-- $ gprbuild -P rule881.gpr&&./bin/rule881
+
 procedure Rule881 is
    Current : Float := Ieee738.Current;
 begin
    Put_Line("FERC Rule 881 Calculator");
-   Put_Line("by Joe Legner 10/21/2022");
+   Put_Line("by Joe Legner 10/25/2022");
+   Put_Line("""A goal without steps is merely a wish.""");
    Put("Current = ");
    Ada.Float_Text_IO.Put(Current, Exp=>0, Aft=>1);
    Put_Line(" amps");
